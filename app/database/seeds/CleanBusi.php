6fd37cd25e4190cb12f76a3157ec44ad55d2fbc6
jhon_chen <jhon.chen@gmail.com>
2014-08-22 09:13:23 +0800

    清除数据类

--- app/database/seeds/CleanBusi.php
+++ app/database/seeds/CleanBusi.php
@@ -3,9 +3,32 @@
 class CleanBusi extends Seeder {
 	public function run()
 	{
-		DB::table('wx_event')->delete();
-		DB::table('sy_accept')->delete();
-		DB::table('wx_complaint')->delete();
-		DB::table('sy_file')->delete();
+ 		DB::table('wx_event')->delete();
+ 		echo "clean wx_event \n";
+ 		DB::table('sy_accept')->delete();
+ 		echo "clean sy_accept \n";
+ 		DB::table('wx_complaint')->delete();
+ 		echo "clean wx_complaint \n";
+ 		DB::table('sy_file')->delete();
+ 		echo "clean sy_file \n";
+		$dir=public_path().DIRECTORY_SEPARATOR.'data';
+		$this->delDirFile($dir);
 	}
+
+	//循环删除目录和文件函数
+	public function delDirFile($dirName){
+		if($handle = opendir("$dirName")){
+			while(false!==($item = readdir($handle))){
+				if($item != "." && $item != ".." ){
+					if(is_dir("$dirName/$item")){
+						delDirAndFile("$dirName/$item" );
+					}else{
+						if(unlink("$dirName/$item"));
+					}
+				}
+			}
+			closedir( $handle );
+		}
+	}
+
 }
